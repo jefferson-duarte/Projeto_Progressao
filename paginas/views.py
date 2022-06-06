ccb75f113e79f566449adddd44e59ca922569cec
Jefferson Duarte <jeffersonsduarte@live.com>
2022-06-06 13:05:21 +0100

    Adicionado templates para o projeto

--- paginas/views.py
+++ paginas/views.py
@@ -2,4 +2,8 @@ from django.views.generic import TemplateView
 
 
 class IndexView(TemplateView):
-    template_name = 'base.html'
+    template_name = 'index.html'
+    
+    
+class SobreView(TemplateView):
+    template_name = 'sobre.html'
